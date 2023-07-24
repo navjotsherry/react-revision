e61d2bf3ed3aa7990acf2fd5031a2500aa79e779
Navjot Sherry <85787077+navjotsherry@users.noreply.github.com>
2023-07-24 11:49:36 -0400

    Chapter 1

---
+++ Chapter-1/App.js
@@ -0,0 +1,5 @@
+const element1 = React.createElement("div",{},[React.createElement("h3",{},"Hi"),React.createElement("h3",{},"Hello there")])
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(element1)
